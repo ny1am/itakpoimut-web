4df3d14841753e997b8dcbfc6625c485235257e6
ny1am <ny1am.lviv.ua@gmail.com>
2017-11-15 16:24:09 +0200

    added about page

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
 import LandingPage from 'views/LandingPage';
+import AboutPage from 'views/AboutPage';
 import NotFoundPage from 'views/NotFoundPage';
 
 import PreloadWrapper from 'components/PreloadWrapper';
 
 export const routeConfig = [{
   path: '/',
+  exact: true,
   component: LandingPage,
+}, {
+  path: '/about',
+  component: AboutPage,
 }];
 
 const Routes = ({ store }) => {
