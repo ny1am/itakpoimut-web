450ff137a8fae2a74c8f2eec461338ede4050767
ny1am <ny1am.lviv.ua@gmail.com>
2017-11-21 15:39:28 +0200

    changed localhost port

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'), // Tells React to build in either dev or prod modes. https://facebook.github.io/react/downloads.html (See bottom)
-      'process.env.API_ROOT': JSON.stringify('http://localhost:5000'),
+      'process.env.API_ROOT': JSON.stringify('http://localhost:5001'),
       __DEV__: true
     }),
     new webpack.HotModuleReplacementPlugin(),
