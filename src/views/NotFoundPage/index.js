67ee6c9f8af62d6e5b606691c9fad1ef365b0960
ny1am <ny1am.lviv.ua@gmail.com>
2017-11-30 15:37:05 +0200

    updated 404 page

--- src/views/NotFoundPage/index.js
+++ src/views/NotFoundPage/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import styles from './styles.scss';
 
 /**
  * 404 page
  */
 const NotFoundPage = () => {
   return (
-    <div className={styles.wrapper}>
-      <h1>
-        404 Page Not Found
-      </h1>
-      <Link to="/"> Go back to homepage </Link>
+    <div className="full-height">
+      <div className="http-error-title">404</div>
+      <div className="http-error-expl">Сторінки не існує</div>
     </div>
   );
 };
