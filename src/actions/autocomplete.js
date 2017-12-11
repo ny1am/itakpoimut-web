d22e01ae4981fa1160ebab93f902eabebaa4be6c
ny1am <ny1am.lviv.ua@gmail.com>
2017-12-11 15:52:32 +0200

    fixed issues with array serialization

--- src/actions/autocomplete.js
+++ src/actions/autocomplete.js
@@ -1,14 +1,16 @@
 import { CALL_API } from 'redux-api-middleware';
+import queryString from 'query-string';
+
 import { API_ROOT } from 'constants';
 import {
   AUTOCOMPLETE_REQUEST, AUTOCOMPLETE_SUCCESS, AUTOCOMPLETE_FAILURE,
 } from 'constants/autocomplete';
-import { removeFalsy, encodeQueryData } from 'utils';
+import { removeFalsy } from 'utils';
 
 export function search({ term, category }) {
   return {
     [CALL_API]: {
-      endpoint: `${API_ROOT}/autocomplete?${encodeQueryData(removeFalsy({ term, category }))}`,
+      endpoint: `${API_ROOT}/autocomplete?${queryString.stringify(removeFalsy({ term, category }))}`,
       method: 'GET',
       types: [
         AUTOCOMPLETE_REQUEST,
--- src/actions/companies.js
+++ src/actions/companies.js
@@ -1,16 +1,19 @@
 import { CALL_API } from 'redux-api-middleware';
+import queryString from 'query-string';
+
 import { API_ROOT } from 'constants';
 import {
   COMPANIES_REQUEST, COMPANIES_SUCCESS, COMPANIES_FAILURE,
 } from 'constants/companies';
-import { removeFalsy, encodeQueryData } from 'utils';
+import { removeFalsy, fixArray } from 'utils';
 
 export function get({ currentPage = 1, sortOrder = 'asc', title, selectedCategory, formData = {} }) {
   //todo
-  const params = Object.assign({}, removeFalsy({ title, selectedCategory }), removeFalsy(formData));
+  let params = Object.assign({}, removeFalsy({ title, selectedCategory }), removeFalsy(formData));
+  params = fixArray(params);
   return {
     [CALL_API]: {
-      endpoint: `${API_ROOT}/companies?currentPage=${currentPage}&sortOrder=${sortOrder}&${encodeQueryData(params)}`,
+      endpoint: `${API_ROOT}/companies?currentPage=${currentPage}&sortOrder=${sortOrder}&${queryString.stringify(params)}`,
       method: 'GET',
       types: [
         COMPANIES_REQUEST,
