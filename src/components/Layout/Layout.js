7ab2340ac5cc9563a6e89d6173bdc07808932925
ny1am <ny1am.lviv.ua@gmail.com>
2017-12-12 11:12:32 +0200

    dialog scroll restoration

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Layout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageYOffset: 0,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.dialogShown && nextProps.dialogShown) {
+      //dialog has been shown
+      this.setState({
+        pageYOffset: window.pageYOffset
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.dialogShown && !this.props.dialogShown) {
+      //dialog has been hidden
+      window.scrollTo(0, prevState.pageYOffset);
+    }
+  }
+
   render() {
     const { children, dialogShown } = this.props;
-    const className = dialogShown ? 'layout-hidden' : '';
+    const { pageYOffset } = this.state;
+    const style = dialogShown ? {
+      overflowX: 'hidden',
+      overflowY: 'scroll',
+      height: '100vh',
+      marginTop: `${-pageYOffset}px`,
+      paddingBottom: `${pageYOffset}px`
+    } : {};
     return (
-      <div className={className}>
+      <div style={style}>
         {children}
       </div>
     );
