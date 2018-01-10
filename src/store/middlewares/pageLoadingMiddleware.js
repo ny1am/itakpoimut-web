62417ad81a86fbfe0caa9ab98e33e286f3bb28e4
ny1am <ny1am.lviv.ua@gmail.com>
2018-01-10 17:44:07 +0200

    fix: not needed console.log

--- src/store/middlewares/pageLoadingMiddleware.js
+++ src/store/middlewares/pageLoadingMiddleware.js
@@ -10,7 +10,9 @@ export default () => next => action => {
   if (action.type === PRELOAD_LOCATION_CHANGE_START && !(action.preloadType === 'dialog' && action.instant)) {
     try {
       Progress.show();
-    } catch(e){console.log(e)} // eslint-disable-line
+    } catch(e) {
+      // do nothing; todo: use another lib for a progress component
+    }
   }
 
   if (action.type === PRELOAD_LOCATION_CHANGE_END && !(action.preloadType === 'dialog' && action.instant)) {
