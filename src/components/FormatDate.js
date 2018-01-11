1b18c7d09832f78900ffd397838da34254c5b718
ny1am <ny1am.lviv.ua@gmail.com>
2018-01-11 15:36:59 +0200

    fix: use standard date manipulation lib

--- src/components/FormatDate.js
+++ src/components/FormatDate.js
@@ -1,21 +1,9 @@
+import dateformat from 'dateformat';
 import PropTypes from 'prop-types';
 
-const leadingZero = (number) => {
-  if (number < 10) {
-    return '0' + number.toString();
-  } else {
-    return number.toString();
-  }
-};
-
 const FormatDate = ({ dateString }) => {
-  const d = new Date(dateString);
-  const dformat = [
-    leadingZero(d.getDate()),
-    leadingZero(d.getMonth()+1),
-    d.getFullYear()
-  ].join('.');
-  return dformat;
+  const date = new Date(dateString);
+  return dateformat(date, 'dd.mm.yyyy');
 };
 
 FormatDate.propTypes = {
