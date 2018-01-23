9116a9a22bf0dae1ad9b8accb6e2174459e33d0c
ny1am <ny1am.lviv.ua@gmail.com>
2018-01-23 10:45:47 +0200

    fix: show 404 on global error

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,19 +2,39 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import PreloadSwitch from 'components/PreloadSwitch';
+import NotFoundPage from 'views/NotFoundPage';
 
 import SecureRoute from './SecureRoute';
 import routeConfig from './routeConfig';
 
-const Routes = () => {
-  return (
-    <PreloadSwitch>
-      {routeConfig.map(cfg => {
-        const RouteComponent = cfg.secure ? SecureRoute : Route;
-        return <RouteComponent key={cfg.path} {...cfg} />;
-      })}
-    </PreloadSwitch>
-  );
-};
+class Routes extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ hasError: false });
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <NotFoundPage />;
+    }
+    return (
+      <PreloadSwitch>
+        {routeConfig.map(cfg => {
+          const RouteComponent = cfg.secure ? SecureRoute : Route;
+          return <RouteComponent key={cfg.path} {...cfg} />;
+        })}
+      </PreloadSwitch>
+    );
+  }
+}
 
 export default Routes;
