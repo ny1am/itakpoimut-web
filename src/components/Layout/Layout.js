799f231385673e8ace38a1eec4d9e5344740894d
ny1am <ny1am.lviv.ua@gmail.com>
2018-01-29 14:03:50 +0200

    fix: scrolling issue on create company dialog

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import disableScroll from 'disable-scroll';
 
 import Header from 'components/Header';
 import Menu from 'components/Menu';
@@ -7,14 +9,40 @@ import Footer from 'components/Footer';
 
 import styles from './styles.scss';
 
-const Layout = ({ overflowShown, children }) => (
-  <div className={`${styles.wrapper} ${overflowShown?styles.fixed:''}`}>
-    <Header />
-    <Menu />
-    {children}
-    <Footer />
-  </div>
-);
+class Layout extends React.Component {
+
+  componentDidMount() {
+    this.scrollHandler();
+  }
+
+  componentDidUpdate() {
+    this.scrollHandler();
+  }
+
+  scrollHandler() {
+    const { overflowShown } = this.props;
+    const element = ReactDOM.findDOMNode(this.refs.layout);
+    if (overflowShown) {
+      disableScroll.on(element, {
+        disableWheel: false,
+      });
+    } else {
+      disableScroll.off(element);
+    }
+  }
+
+  render() {
+    const { overflowShown, children } = this.props;
+    return (
+      <div ref="layout" className={`${styles.wrapper} ${overflowShown?styles.fixed:''}`}>
+        <Header />
+        <Menu />
+        {children}
+        <Footer />
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   overflowShown: PropTypes.bool.isRequired,
