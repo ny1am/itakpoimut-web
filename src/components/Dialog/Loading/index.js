1027e7808994a1c597ec4d84d033b9ee4df85daa
ny1am <ny1am.lviv.ua@gmail.com>
2018-02-18 15:44:13 +0200

    fix: dialog loading on small viewports

--- src/components/Dialog/Loading/index.js
+++ src/components/Dialog/Loading/index.js
@@ -4,11 +4,13 @@ import styles from './styles.scss';
 
 const Loading = () => (
   <section className={styles.container}>
-    <div className={styles.wrapper}>
-      <div className={styles.block} />
-      <div className={`${styles.block} ${styles.delay}`} />
-      <div className={`${styles.block} ${styles.delay}`} />
-      <div className={styles.block} />
+    <div className={styles.viewportCenterer}>
+      <div className={styles.wrapper}>
+        <div className={styles.block} />
+        <div className={`${styles.block} ${styles.delay}`} />
+        <div className={`${styles.block} ${styles.delay}`} />
+        <div className={styles.block} />
+      </div>
     </div>
   </section>
 );
